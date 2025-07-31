15352d8a35be83d0f74deed346167ea8546cd76e
Arpit0008 <arpitupadhyay169@gmail.com>
2025-07-31 21:03:15 +0530

    Update About, Contact, Footer pages and add profile image

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -3,9 +3,11 @@ import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import me from "../../../images/me.png";
+
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://instagram.com/arpitupadhyay._.08";
   };
   return (
     <div className="aboutSection">
@@ -18,28 +20,27 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src={me}
               alt="Founder"
             />
-            <Typography>Abhishek Singh</Typography>
+            <Typography>Arpit Upadhyay</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+              This is a sample wesbite made by Arpit Upadhyay. 
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              href="https://www.youtube.com"
               target="blank"
             >
               <YouTubeIcon className="youtubeSvgIcon" />
             </a>
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
+            <a href="https://instagram.com/arpitupadhyay._.08" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
             </a>
           </div>
--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,14 +17,14 @@ const Footer = () => {
         <h1>ECOMMERCE.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; Arpit Upadhyay</p>
       </div>
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="http://instagram.com/arpitupadhyay._.08">Instagram</a>
+        <a href="http://youtube.com">Youtube</a>
+        <a href="http://instagram.com/arpitupadhyay._.08">Facebook</a>
       </div>
     </footer>
   );
